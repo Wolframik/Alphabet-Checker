09a4e71bf22203b6c657dc8610d9dc063ec57f48
Alex <wolframik.33@gmail.com>
2025-09-29 21:58:25 +0200

    Now the p 'alphabet' is dynamic and updates the aplhabet so that i can see all the missing alphabet letter

--- script.js
+++ script.js
@@ -4,10 +4,28 @@ document.addEventListener("DOMContentLoaded", () =>
 
     textarea.addEventListener("input", () => 
     {
-        const value = textarea.value;
         console.log("Textarea updated:", textarea.value);
-
-
+        CheckAlphabet(textarea.value);
     });
-
 });
+
+function CheckAlphabet(text)
+{
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    const seen = new Set();
+
+  for (const char of text.toLowerCase()) 
+    {
+        if (alphabet.includes(char)) 
+        {
+            seen.add(char);
+        }
+    }
+
+  // Find missing letters
+  const missing = [...alphabet].filter(letter => !seen.has(letter));
+
+  const p = document.getElementById("alphabet");
+  p.textContent = missing.join('');
+
+}
